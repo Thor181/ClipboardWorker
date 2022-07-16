73f470cfd0604e46469fcb0865df19c1c61b14bc
Thor181 <art.torgaschin@yandex.ru>
2022-07-16 19:31:44 +0700

    Fix Command

---
+++ ViewModels/Command.cs
@@ -0,0 +1,39 @@
+﻿using ClipboardWorker.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ClipboardWorker.ViewModels
+{
+    internal class Command : ICommand
+    {
+        public Action<object> action;
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter != null)
+            {
+                this.action.Invoke(parameter);
+            }
+        }
+
+        public Command(Action<object> action)
+        {
+            this.action = action;
+        }
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -31,10 +31,6 @@ namespace ClipboardWorker.ViewModels
             }
         }
 
-        
-
-
-
         public delegate void SetupClipboardEventHandler(object? sender, PropertyChangedEventArgs e, ItemModel itemModel);
         public event SetupClipboardEventHandler? PropertyChanged;
 
@@ -45,10 +41,28 @@ namespace ClipboardWorker.ViewModels
                 PropertyChanged(this, new PropertyChangedEventArgs(propName), value);
             }
         }
+        private Command _removeItemCommand;
+        public Command RemoveItemCommand
+        {
+            get
+            {
+                if (_removeItemCommand == null)
+                {
+                    _removeItemCommand = new Command((SelectedItem) => { Items.Remove((ItemModel)SelectedItem); });
+                }
+                return _removeItemCommand;
+            }
+            private set
+            {
+                _removeItemCommand = value;
+            }
+        }
 
         private void MainViewModel_SetupClipboard(object? sender, PropertyChangedEventArgs e, ItemModel itemModel)
         {
+            if (itemModel == null) return;
             Clipboard.SetText(itemModel.Text);
+
         }
 
         public MainViewModel()
@@ -56,10 +70,12 @@ namespace ClipboardWorker.ViewModels
             Items = new ObservableCollection<ItemModel>();
             _mainWindow = (MainWindow)System.Windows.Application.Current.MainWindow;
             PropertyChanged += MainViewModel_SetupClipboard;
+            
             UpdateClipboard();
         }
         private void UpdateClipboard()
         {
+            
             Thread thread = new Thread(() =>
             {
                 while (true)
